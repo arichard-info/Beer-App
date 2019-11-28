6bf78df4a503ca52e8c3dca0aa3611df6b5af58d
Antoine Richard <antoine.richard.partner@decathlon.com>
2019-11-28 14:03:14 +0100

    remove console.log

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -3,7 +3,6 @@ const Beer = mongoose.model("Beer");
 
 exports.getDefault = async (req, res, next) => {
   const beers = await Beer.find();
-  console.log(beers);
   res.header("Content-Type", "application/json");
   res.send(JSON.stringify({ beers, test: "xDD" }));
 };
