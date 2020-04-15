ca3527c50437df757c22ee8d77639df8af520c77
arichard <antoiner.info@gmail.com>
2020-04-15 21:08:36 +0200

    Task : Refacto Webpack Client

---
+++ client/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const DotenvWebpack = require("dotenv-webpack");
+
+module.exports = {
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new DotenvWebpack({
+      path: ".env",
+    }),
+  ],
+};
