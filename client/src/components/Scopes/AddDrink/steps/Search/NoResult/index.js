a49876d8b4844cb479a87199eab800c2228cdcc5
Antoine Richard <antoiner.info@gmail.com>
2020-12-01 23:08:38 +0100

    feat(addDrink): mobile display & not found beer style

---
+++ client/src/components/Scopes/AddDrink/steps/Search/NoResult/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import BeerIcon from "./BeerIcon";
+
+const NoResult = ({ className, onClick }) => {
+  return (
+    <div className={className}>
+      <div className="content">
+        <h3>Bière inconnue ?</h3>
+        <p>Appuie là dessus pour saisir ses caractéristiques</p>
+        <button onClick={onClick}>
+          <BeerIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default styled(NoResult)(
+  ({ theme: { colors, fw, device } }) => css`
+    .content  {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    h3 {
+      font-size: 1.4rem;
+      font-weight: 600;
+      margin-bottom: 1rem;
+      margin-top: 4rem;
+    }
+    p {
+      margin: 0;
+    }
+    button {
+      background-color: transparent;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      transition: all 0.2s ease;
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
+
+    @media ${device.gtMobile} {
+      h3 {
+        font-size: 1.6rem;
+      }
+    }
+  `
+);
