14a0e5f4500b492225742c30356296bb59036b73
Antoine Richard <antoiner.info@gmail.com>
2020-12-11 01:10:03 +0100

    fix: react-is dependency

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -25,46 +25,46 @@ const Layout = ({ className, loggedIn, children }) => {
 export default styled(Layout)(
   ({ loggedIn, theme: { device, colors } }) => css`
     ${loggedIn &&
-      css`
+    css`
+      position: relative;
+      z-index: 0;
+      & > header {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background-color: ${colors.white};
+        z-index: 1;
+        padding: 2rem 0;
+      }
+      @media ${device.gtMobile} {
+        max-width: 90rem;
+        margin-left: auto;
+        margin-right: auto;
         position: relative;
-        z-index: 0;
         & > header {
-          position: fixed;
-          bottom: 0;
-          left: 0;
-          width: 100%;
-          background-color: ${colors.white};
-          z-index: 1;
-          padding: 2rem 0;
+          width: calc(90rem / 3);
+          top: 0;
+          left: initial;
+          border-right: 0.1rem solid ${colors.grey1};
         }
-        @media ${device.gtMobile} {
-          max-width: 90rem;
-          margin-left: auto;
-          margin-right: auto;
-          position: relative;
-          & > header {
-            width: calc(90rem / 3);
-            top: 0;
-            left: initial;
-            border-right: 0.1rem solid ${colors.grey1};
-          }
-          & > main {
-            width: 66.66%;
-            margin-left: 33.33%;
-          }
+        & > main {
+          width: 66.66%;
+          margin-left: 33.33%;
         }
-      `}
+      }
+    `}
 
     ${!loggedIn &&
-      css`
-        height: 100%;
-        max-width: 600px;
-        width: 100%;
-        margin: auto;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        padding: 4rem;
-      `}
+    css`
+      height: 100%;
+      max-width: 600px;
+      width: 100%;
+      margin: auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding: 4rem;
+    `}
   `
 );
