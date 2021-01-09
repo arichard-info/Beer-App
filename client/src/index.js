501acf35333cd661b24d1a37d836029df26b0287
Antoine Richard <56544866+arichard-info@users.noreply.github.com>
2021-01-09 01:37:51 +0100

    fix(frontend): Remove service worker (#493)
    Co-authored-by: Kalagounet <antoiner.info@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "@/components/App";
-import * as serviceWorker from "@/serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
