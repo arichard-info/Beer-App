682336556b6b6cdac94c185711d2413b05f9cd8d
Antoine Richard <antoiner.info@gmail.com>
2021-02-10 13:49:38 +0100

    continue slider component

--- client/src/components/Global/RangeSlider/index.js
+++ client/src/components/Global/RangeSlider/index.js
@@ -1,4 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import { classNames } from "@/utils";
 
 const RangeSlider = ({
   onChange = () => {},
@@ -7,9 +9,17 @@ const RangeSlider = ({
   max = 10,
   className,
 }) => {
-  const handleChange = () => {};
+  const [dragging, setDragging] = useState(false);
 
-  const handleMouseUp = () => {};
+  const handleChange = () => {
+    console.log("change");
+  };
+
+  const handleMouseUp = () => {
+    console.log("up");
+    removeEventListeners();
+    setDragging(false);
+  };
 
   const handleTouchStart = () => {
     window.addEventListener("touchend", handleMouseUp);
@@ -18,7 +28,9 @@ const RangeSlider = ({
   const handleTouchMove = () => {};
 
   const handleMouseDown = () => {
+    setDragging(true);
     window.addEventListener("mouseup", handleMouseUp);
+    window.addEventListener("mousemove", handleChange);
   };
 
   const handleMouseOver = () => {};
@@ -33,38 +45,58 @@ const RangeSlider = ({
     return removeEventListeners;
   });
   return (
-    <div
-      className={className}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onMouseDown={handleMouseDown}
-      onMouseOver={handleMouseOver}
-    >
-      <div className="marker" style={{ bottom: "50%" }} />
-      <div className="track" />
+    <div className={className}>
+      <div
+        className={classNames("track", { dragging })}
+        // onTouchStart={handleTouchStart}
+        // onTouchMove={handleTouchMove}
+        onMouseDown={handleMouseDown}
+        // onMouseOver={handleMouseOver}
+      >
+        <div
+          className="progress"
+          style={{ height: `${(100 * value) / (max - min)}%` }}
+        />
+        <div
+          className="marker"
+          style={{ bottom: `${(100 * value) / (max - min)}%` }}
+        />
+      </div>
     </div>
   );
 };
 
 export default styled(RangeSlider)(
-  () => css`
-    width: 100%;
-    position: relative;
+  ({ theme: { colors } }) => css`
+    height: 100%;
     .track {
       height: 100%;
       width: 1rem;
       background-color: #e9e9e9;
       border-radius: 2rem;
+      position: relative;
+      cursor: pointer;
+      &.dragging {
+        cursor: move;
+      }
     }
-    .market {
+    .marker {
       position: absolute;
       left: 50%;
-      transform: translateX(-50%);
-      height: 1rem;
-      width: 1rem;
+      transform: translate(-50%, 50%);
+      height: 1.2rem;
+      width: 1.2rem;
       border-radius: 50%;
       background-color: #ffffff;
       border: 0.1rem solid #000000;
+    }
+    .progress {
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background-color: ${colors.primary};
+      border-radius: 2rem;
     }
   `
 );
--- client/src/components/Scopes/Profile/index.js
+++ client/src/components/Scopes/Profile/index.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 
 import RangeSlider from "@/components/Global/RangeSlider";
 
 const ProfilePage = ({ className }) => {
+  const [value, setValue] = useState(25);
   return (
     <div className={className}>
       <h1>Profile page</h1>
-      <div>
-        <RangeSlider />
+      <div style={{ height: "20rem" }}>
+        <RangeSlider min={0} max={100} value={value} onChange={setValue} />
       </div>
     </div>
   );
 };
 
-export default styled(ProfilePage)(() => css``);
+export default styled(ProfilePage)(
+  () => css`
+    padding-left: 1.5rem;
+  `
+);
