ea8c7c9aed7caa8c869d49170ee780ccbb2a8166
Antoine Richard <56544866+arichard-info@users.noreply.github.com>
2021-05-08 00:35:01 +0200

    feat(tests): Remove data-nrt on production (#698)
    * feat(tests): Remove data-nrt on production
    
    * feat: dynamic babel configuration
    
    * try ignoring cypress path
    
    * test forcing value
    
    * test forcing value in dockerfile
    
    * test CMD dockerfile
    
    * test context explicit in docker-compose
    
    * test forcing context in dockerfile
    
    * test forcing context in dockerfile
    
    * test forcing context in dockerfile
    
    * try other option for RUN
    
    * try other option for RUN
    
    * try other option for RUN
    
    * try using args
    
    * try using args
    
    * try using only env variable
    
    * fix context
    
    Co-authored-by: Antoine RICHARD <antoiner.info@gmail.com>

---
+++ client/babel.config.js
@@ -0,0 +1,10 @@
+const showDataNrt =
+  process.env.CONTEXT === "test" || process.env.NODE_ENV === "development";
+
+module.exports = {
+  presets: ["react-app"],
+  plugins: [
+    !showDataNrt && ["react-remove-properties", { properties: ["data-nrt"] }],
+  ].filter(Boolean),
+  ignore: ["cypress"],
+};
